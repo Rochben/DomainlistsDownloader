8b74d7d92941ed3642a1838da229bfb0d9475438
Rochben <Rochben@users.noreply.github.com>
2020-08-21 10:34:14 +0200

    Add files via upload

---
+++ DL.py
@@ -0,0 +1,19 @@
+import requests, os, csv, getpass
+
+# Input variables
+email = input('Please type in your email address: ')
+password = getpass.getpass('Please type in your password: ')
+list_type = input('Please select the list type (full, new, deled, fulldns, newdns, deleteddns): ') or 'full'
+zone_code = input('Please type in a zone ID or all_zones: ') or 'all_zones'
+
+# File variables
+desktop = os.path.normpath(os.path.expanduser("~/Desktop"))
+filename = f'Domainlists_Zone_{zone_code}_{list_type.capitalize()}.csv'
+path = f"{desktop}/{filename}"
+url = f"https://domainlists.io/api/{list_type}/{zone_code}/{email}/{password}/"
+
+# Writing .csv
+response = requests.get(url)
+with open(f'{path}', 'wb') as n:
+    r = (response)
+    n.write(r.content)
